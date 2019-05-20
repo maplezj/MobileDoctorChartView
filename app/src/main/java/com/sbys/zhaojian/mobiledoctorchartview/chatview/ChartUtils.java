914253bd4f7382310f72a2052fecda46efbb6b84
zhaojian <zhaojian1@winning.com.cn>
2019-05-20 16:29:59 +0800

    日期格式修改

--- app/src/main/java/com/sbys/zhaojian/mobiledoctorchartview/chatview/ChartUtils.java
+++ app/src/main/java/com/sbys/zhaojian/mobiledoctorchartview/chatview/ChartUtils.java
@@ -52,7 +52,7 @@ public class ChartUtils
     {
         if (!TextUtils.isEmpty(dateString)) {
             try {
-                return getDateString(getDate(dateString, "M/d HH:mm"), "M-d");
+                return getDateString(getDate(dateString, "yyyy-MM-dd HH:mm"), "MM-dd");
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -64,7 +64,7 @@ public class ChartUtils
     {
         if (!TextUtils.isEmpty(dateString)) {
             try {
-                return getDateString(getDate(dateString, "M/d HH:mm:ss"), "HH:mm:ss");
+                return getDateString(getDate(dateString, "yyyy-MM-dd HH:mm"), "HH:mm:ss");
             } catch (Exception e) {
                 e.printStackTrace();
             }
